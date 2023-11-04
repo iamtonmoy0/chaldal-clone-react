ffef23d8490115fae81df0c9055297a1cdaf962a
iamtonmoy0 <tonmoy2code@gmail.com>
2023-11-04 14:13:37 -0400

    starter

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { DatePicker } from "antd"
+
+function App() {
+
+  return (
+    <>
+    <DatePicker  />
+    <p className="text-4xl text-center">date</p>
+    </>
+  )
+}
+
+export default App
