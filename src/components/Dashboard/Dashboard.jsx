1932abb77f1c9760f7604bf7c6ae00d937f75571
iamtonmoy0 <tonmoy2code@gmail.com>
2024-05-02 16:10:20 +0600

    "Add Ant Design icons, navigation bar, and side menu to Dashboard component"
    This commit adds the @ant-design/icons package and uses it to include various icons in the Dashboard component. It also includes a navigation

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,3 +1,76 @@
+import {
+  AppstoreOutlined,
+  BarChartOutlined,
+  CloudOutlined,
+  ShopOutlined,
+  TeamOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from "@ant-design/icons";
+import { Layout, Menu } from "antd";
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "../Navbar/Navbar";
+const { Header, Sider } = Layout;
+const items = [
+  UserOutlined,
+  VideoCameraOutlined,
+  UploadOutlined,
+  BarChartOutlined,
+  CloudOutlined,
+  AppstoreOutlined,
+  TeamOutlined,
+  ShopOutlined,
+].map((icon, index) => ({
+  key: String(index + 1),
+  icon: React.createElement(icon),
+  label: `nav ${index + 1}`,
+}));
+
+const headerStyle = {
+  textAlign: "center",
+  color: "#fff",
+  height: 64,
+  paddingInline: 48,
+  lineHeight: "64px",
+  backgroundColor: "#4096ff",
+};
+
+const layoutStyle = {
+  overflow: "hidden",
+  width: "100%",
+  maxWidth: "100%",
+  minHeight: "100vh",
+};
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  return (
+    <div className="h-full">
+      <Layout style={layoutStyle}>
+        <Navbar />
+        <Layout>
+          <Sider
+            style={{
+              overflow: "auto",
+              height: "calc(100vh - 64px)",
+              position: "fixed",
+              left: 0,
+              top: 64,
+              bottom: 0,
+            }}
+          >
+            <div className="demo-logo-vertical" />
+            <Menu
+              theme="dark"
+              mode="inline"
+              defaultSelectedKeys={["4"]}
+              items={items}
+            />
+          </Sider>
+          {/* dynamic render space */}
+          <Outlet />
+        </Layout>
+      </Layout>
+    </div>
+  );
 }
---
+++ src/pages/Welcome/Welcome.jsx
@@ -0,0 +1,5 @@
+"react";
+
+export default function Welcome() {
+  return <div>Welcome</div>;
+}
